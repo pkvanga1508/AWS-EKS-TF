2974cbe5c7072d571dc444f9095e7e5b2f74ebbd
praveen <pkvanga1508@gmail.com>
2025-04-09 12:31:49 -0700

    02-providers-resources

--- 02-Terraform-Settings-providers-resources/terraform-manifests/c2-ec2instance.tf
+++ 02-Terraform-Settings-providers-resources/terraform-manifests/c2-ec2instance.tf
@@ -1,5 +1,8 @@
-#Resource: EC2 Instance
-resource "aws_instance" "ec2-instance" {
-   ami = ""
-
+resource "aws_instance" "my_ec2_vm" {
+  ami           = "ami-04985531f48a27ae7" # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+  user_data     = file("${path.module}/app1-install.sh")
+  tags = {
+    "name" = "EC2 Demo"
+  }
 }
